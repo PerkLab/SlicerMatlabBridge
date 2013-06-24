5a0a6e9c1f676850b3deb8b68d40c9c27c74ff3d
lassoan <lassoan@25a7314f-c4c8-4314-9936-a25f8480a772>
2013-06-24 17:01:29 +0000

    re #326: Simplified MatlabModuleTemplate.m
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/MatlabBridge/src@937 25a7314f-c4c8-4314-9936-a25f8480a772

--- MatlabModuleGenerator/Resources/Templates/MatlabModuleTemplate.m
+++ MatlabModuleGenerator/Resources/Templates/MatlabModuleTemplate.m
@@ -1,55 +1,20 @@
 function outputParams=MatlabModuleTemplate(inputParams)
-
-%% Getting inputs
+% Example function that returns the minimum and maximum voxel value in a volume
+% and performs thresholding operation on the volume.
 %
-% Named parameters: inputParams.paramName
-% Unnamed parameters: inputParams.unnamed(1), inputParams.unnamed(2), ...
+% Parameters:
+%  inputParams.threshold: threshold value
+%  inputParams.unnamed{1}: input image filename
+%  inputParams.unnamed{2}: output image filename (value is 0 if below threshold, 100 if above threshold)
+%  outputParams.min: image minimum value
+%  outputParams.max: image maximum value
 %
-% For integer, integer-vector, integer-enumeration, float, float-vector, float-enumeration, double, double-vector, double-enumeration, point, region, string, string-enum:
-%   paramValue=inputParams.paramName;
-% For boolean:
-%   paramValue=isfield(inputParams,'paramName');
-% For string-vector:
-%   paramValue=strsplit(inputParams.paramName,',');
-% For file:
-%   f=fopen(inputParams.paramName, ...);
-%   ...
-%   fclose(f);
-% For directory:
-%   ???
-% For image:
-%   img=nrrdread(inputParams.paramName);
-% Geometry:
-%   geom=stlread(inputParams.paramName);
-%
-
-%% Writing outputs
-%
-% For integer, integer-vector, integer-enumeration, float, float-vector, float-enumeration, double, double-vector, double-enumeration, point, region, string, string-enum, boolean:
-%   outputParams.paramName=paramValue;
-% For string-vector:
-%   outputParams.paramName=strjoin(paramValue,',');
-% For file:
-%   f=fopen(inputParams.paramName, ...);
-%   ...
-%   fclose(f);
-% For directory:
-%   ???
-% For image (note that for processing you may need to convert the image type to double and it is advisable to convert back to the original data type before writing):
-%   nrrdwrite(inputParams.paramName, img);
-% Geometry:
-%   stlwrite(inputParams.paramName, geom);
-%
-% If there are no output parameters:
-%   outputParams=[];
-
-%% Example
 
 img=cli_imageread(inputParams.unnamed{1});
 
 outputParams.min=min(min(min(img.pixelData)));
 outputParams.max=max(max(max(img.pixelData)));
 
-img.pixelData=(double(img.pixelData)>inputParams.threshold)*100+50;
+img.pixelData=(double(img.pixelData)>inputParams.threshold)*100;
 
 cli_imagewrite(inputParams.unnamed{2}, img);
