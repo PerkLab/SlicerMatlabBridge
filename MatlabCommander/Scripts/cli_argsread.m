3120fc1a630c4f43ced54f3eb4820c90660b7635
lassoan <lassoan@25a7314f-c4c8-4314-9936-a25f8480a772>
2013-06-27 20:20:15 +0000

    re #335: Added support for reading fiducial lists (can be accessed as inputParams.fiducials{1}, inputParams.fiducials{2}, etc.)
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/MatlabBridge/src@948 25a7314f-c4c8-4314-9936-a25f8480a772

--- MatlabCommander/Scripts/cli_argsread.m
+++ MatlabCommander/Scripts/cli_argsread.m
@@ -3,8 +3,7 @@ function params=cli_argsread(args)
 % The output structure contains all the named arguments (field name is the command-line argument name)
 % and an "unnamed" argument containing the list of unnamed arguments in a cell.
 
-% Add an empty argument at the end to make sure we flush all the partially processed arguments
-args{length(args)+1}='--';
+args=fixupArgumentList(args);
 
 params={};
 params.unnamed={};
@@ -14,38 +13,49 @@ curArgValue='';
 for curArgIndex=1:length(args)
     if (regexp(args{curArgIndex},'^-[-]?[a-zA-Z]')==1)
         % This is an argument name (as it starts with - or -- followed by letters)
-        if (~isempty(curArgName))
-            % There is an argument already, so store it
-            params.(curArgName)=curArgValue;
-            curArgName='';
-            curArgValue='';            
-        else
-            % There is no argument name, only argument value, so it's an
-            % unnamed argument
-            if (~isempty(curArgValue))
-                params.unnamed{length(params.unnamed)+1}=curArgValue;
-                curArgValue='';
-            end
-        end
-        % Save the new argument name
-        if (args{curArgIndex}(2)=='-')
-            % long flag (--argName)
-            curArgName=args{curArgIndex}(3:end);
-        else
-            % long flag (-argName)
-            curArgName=args{curArgIndex}(2:end);
-        end
-        continue;
+        % There may be an argument name without a value (flag parameter) or value
+        % without a name (unnamed parameter) => save it before we start
+        % processing the new argument
+        params=saveParam(params,curArgName,curArgValue);
+        curArgName='';
+        curArgValue='';       
+        % Save the new argument name (but don't store it yet, it may be
+        % followed by an argument value)
+        % Strip the - or -- from the argument name
+        [tokens matches]=regexp(args{curArgIndex},'^-[-]?([^-]+)','tokens','match');
+        curArgName=tokens{1}{1};
     else
-        % this is an argument value
+        % This is an argument value
         curArgValue=getValueFromString(args{curArgIndex});
-        if (isempty(curArgName))
-            params.unnamed{length(params.unnamed)+1}=curArgValue;
-            curArgValue='';
-        else
-            params.(curArgName)=curArgValue;
-            curArgName='';
-            curArgValue='';
+        % We have a pair of argument name and value => save it
+        params=saveParam(params,curArgName,curArgValue);
+        curArgName='';
+        curArgValue='';
+    end
+end
+
+% If there is an argument that has not yet stored, store it
+params=saveParam(params,curArgName,curArgValue);
+
+end
+
+%% Helper functions
+
+% Fixing up the parameter list
+% Fiducial point name and value is stored in one argument:
+%  '--somefiducial 12.3,14.6, 18.7'
+% Detect this case and convert it to
+%  '--somefiducial' '12.3,14.6, 18.7'
+function updatedArgs=fixupArgumentList(originalArgs)
+    updatedArgs={};
+    for curArgIndex=1:length(originalArgs)
+        if (regexp(originalArgs{curArgIndex},'^-[-]?[a-zA-Z].*[ ].+')==1)
+            % Detected a space in an argument name, e.g., '--somefiducial 12.3,14.6,18.7'
+            [matchedTokens matchedString]=regexp(originalArgs{curArgIndex},'^(-[-]?[a-zA-Z]+[^ ]*)[ ](.+)','tokens','match');
+            argName=matchedTokens{1}{1}; % '--somefiducial'
+            argValue=matchedTokens{1}{2}; % '12.3,14.6,18.7'
+            % Insert the split argName and argValue in the argument list
+            updatedArgs=[updatedArgs {argName} {argValue}];
         end
     end
 end
@@ -59,3 +69,20 @@ function paramValue=getValueFromString(paramString)
   end
   % There was an error, so we interpret this value as a string
   paramValue=paramString;
+end
+
+function updatedParams=saveParam(originalParams,curArgName,curArgValue)
+    updatedParams=originalParams;   
+    if (isempty(curArgName))        
+        if (~isempty(curArgValue))
+            updatedParams.unnamed{length(updatedParams.unnamed)+1}=curArgValue;
+        end
+    else
+        if (isfield(updatedParams, curArgName))
+            % This field exists already, so add the value
+            updatedParams.(curArgName)=[updatedParams.(curArgName) {curArgValue}];
+        else
+            updatedParams.(curArgName)=curArgValue;
+        end
+    end
+end
