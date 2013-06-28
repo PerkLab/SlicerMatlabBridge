04842581782327f03b01bbf208ab5d28477035f0
lassoan <lassoan@25a7314f-c4c8-4314-9936-a25f8480a772>
2013-06-28 18:49:49 +0000

    re #331: Added some help into the skeleton .m file about how to read/write various parameter types
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/MatlabBridge/src@954 25a7314f-c4c8-4314-9936-a25f8480a772

--- MatlabModuleGenerator/Resources/Templates/MatlabModuleTemplate.m
+++ MatlabModuleGenerator/Resources/Templates/MatlabModuleTemplate.m
@@ -5,7 +5,7 @@ function outputParams=MatlabModuleTemplate(inputParams)
 % Parameters:
 %  inputParams.threshold: threshold value
 %  inputParams.unnamed{1}: input image filename
-%  inputParams.unnamed{2}: output image filename (value is 0 if below threshold, 100 if above threshold)
+%  inputParams.unnamed{2}: output image filename, result of the processing
 %  outputParams.min: image minimum value
 %  outputParams.max: image maximum value
 %
@@ -18,3 +18,39 @@ outputParams.max=max(max(max(img.pixelData)));
 img.pixelData=(double(img.pixelData)>inputParams.threshold)*100;
 
 cli_imagewrite(inputParams.unnamed{2}, img);
+
+%% Help
+%
+% Reading input parameters
+%
+%  integer, integer-vector, float, float-vector, double, double-vector, string, string-enumeration, file, point:
+%    value=inputParams.name;
+%  string-vector:
+%    value=cli_stringvectordecode(inputParams.name);
+%  boolean:
+%    value=isfield(inputParams,'name');
+%  image:
+%    value=cli_imageread(inputParams.name);
+%  transform:
+%    value=cli_lineartransformread(inputParams.name);
+%   or (for generic transforms):
+%    value=cli_transformread(inputParams.name);
+%  measurement:
+%    value=cli_measurementread(inputParams.name);
+%
+%  Note: for retrieving indexth-th unnamed parameter use inputParams.unnamed{index+1} instead of inputParams.name
+%
+%
+% Writing output parameters
+%
+%  integer, integer-vector, float, float-vector, double, double-vector, string, string-enumeration, file:
+%    outputParams.name=value;
+%  image:
+%    cli_imagewrite(inputParams.name, value);
+%  transform:
+%    cli_lineartransformwrite(inputParams.name, value);
+%   or (for generic transforms):
+%    cli_transformwrite(inputParams.name, value);
+%  measurement:
+%    cli_measurementwrite(inputParams.name, value);
+%
