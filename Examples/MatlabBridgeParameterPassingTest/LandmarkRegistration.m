628310972229b0ae99a2fe8e6a56973ab6cb2079
lassoan <lassoan@25a7314f-c4c8-4314-9936-a25f8480a772>
2013-06-29 14:04:15 +0000

    re #331: Add more MatlabBridge examples
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/MatlabBridge/src@959 25a7314f-c4c8-4314-9936-a25f8480a772

---
+++ Examples/MatlabBridgeParameterPassingTest/LandmarkRegistration.m
@@ -0,0 +1,13 @@
+function outputParams=LandmarkRegistration(inputParams)
+% Example function that computes a rigid similarity transform between two sets of points
+
+fixedPoints=cli_pointvectordecode(inputParams.fixedpoints);
+movingPoints=cli_pointvectordecode(inputParams.movingpoints);
+doScale=isfield(inputParams,'enablescaling');
+
+[regParams,Bfit,ErrorStats]=absor(fixedPoints',movingPoints','doScale',doScale);
+
+outputParams.averageerror=ErrorStats.errlsq;
+outputParams.maxerror=ErrorStats.errmax;
+
+cli_lineartransformwrite(inputParams.movingtofixedtransform,regParams.M);
