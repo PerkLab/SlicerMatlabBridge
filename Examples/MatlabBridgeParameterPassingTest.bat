7437ef942ab18d2a9db489e82ba6d45e5c9f9cb7
lassoan <lassoan@25a7314f-c4c8-4314-9936-a25f8480a772>
2013-06-29 12:39:22 +0000

    re #331: Added example Matlab module with all possible input and output parameter types
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/MatlabBridge/src@958 25a7314f-c4c8-4314-9936-a25f8480a772

---
+++ Examples/MatlabBridgeParameterPassingTest.bat
@@ -0,0 +1,24 @@
+@set MODULE_NAME=MatlabBridgeParameterPassingTest
+@rem MatlabBridge proxy version: 1.0
+
+@echo off
+if not "%~1"=="--xml" goto :launch_module
+
+:print_xml
+rem Print CLI descriptor XML
+type %MODULE_NAME%.xml
+rem Return with success
+exit /b 0
+
+:launch_module
+rem Print error messages if any of the required environment variables are not set
+if not defined SLICER_MATLAB_COMMANDER_PATH echo ERROR: SLICER_MATLAB_COMMANDER_PATH environment variable is not defined. Make sure you installed the MatlabBridge extension.
+if not defined SLICER_MATLAB_COMMAND_SERVER_SCRIPT_PATH echo ERROR: SLICER_MATLAB_COMMAND_SERVER_SCRIPT_PATH environment variable is not defined. Make sure you installed the MatlabBridge extension.
+if not defined SLICER_MATLAB_EXECUTABLE_PATH echo ERROR: SLICER_MATLAB_EXECUTABLE_PATH environment variable is not defined. Make sure you installed the MatlabBridge extension and set the path to the Matlab executable.
+rem Make the .bat file location to be the working directory (that's where the MatlabCommander.exe is)
+pushd "%~dp0"
+rem Forward parameters to the Matlab CLI
+%SLICER_HOME%/Slicer.exe --launcher-no-splash --launch %SLICER_MATLAB_COMMANDER_PATH% --call-matlab-function %MODULE_NAME% %*
+if errorlevel 1 exit /b 1
+rem Return with success
+exit /b 0
---
+++ Examples/MatlabBridgeParameterPassingTest.m
@@ -0,0 +1,43 @@
+function outputParams=MatlabBridgeParameterPassingTest(inputParams)
+% Example function that returns the minimum and maximum voxel value in a volume
+% and performs thresholding operation on the volume.
+%
+% Parameters:
+%  inputParams.threshold: threshold value
+%  inputParams.unnamed{1}: input image filename
+%  inputParams.unnamed{2}: output image filename (value is 0 if below threshold, 100 if above threshold)
+%  outputParams.min: image minimum value
+%  outputParams.max: image maximum value
+%
+
+% Read inputs
+integerValue=inputParams.integer
+doubleValue=inputParams.double
+floatVectorValue=inputParams.f
+stringVectorValue=cli_stringvectordecode(inputParams.string_vector)
+stringEnumerationValue=inputParams.enumeration
+boolean1Value=isfield(inputParams,'boolean1')
+boolean2Value=isfield(inputParams,'boolean2')
+boolean3Value=isfield(inputParams,'boolean3')
+filenameValue=inputParams.file1
+filenameListValue=cli_stringvectordecode(inputParams.files)
+directoryValue=cli_stringvectordecode(inputParams.directory1)
+imageValue=cli_imageread(inputParams.image1)
+transformValue=cli_lineartransformread(inputParams.transform1)
+seedValue=inputParams.seed
+inputMeasurementFilenameValue=cli_measurementread(inputParams.inputFA)
+unnamedInputImageValue=cli_imageread(inputParams.unnamed{1});
+
+% Write outputs
+outputParams.anintegerreturn=integerValue*10;
+outputParams.abooleanreturn=boolean1Value && boolean2Value && boolean3Value;
+outputParams.afloatreturn=123.456;
+outputParams.adoublereturn=246.81012;
+outputParams.astringreturn=['the input was ' stringEnumerationValue];
+outputParams.anintegervectorreturn=floatVectorValue*2;
+outputParams.astringchoicereturn=stringEnumerationValue;
+
+cli_imagewrite(inputParams.image2, imageValue);
+cli_lineartransformwrite(inputParams.transform2,transformValue);
+cli_imagewrite(inputParams.unnamed{2},unnamedInputImageValue);
+cli_measurementwrite(inputParams.outputFA, [inputMeasurementFilenameValue; 1 2 0.2; 2 4 0.3; 3 5 0.2]);
