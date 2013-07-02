810e088af20df929a546da879996dc388a03bde2
lassoan <lassoan@25a7314f-c4c8-4314-9936-a25f8480a772>
2013-07-02 17:55:09 +0000

    test #345: Fix cli_pointvectordecode for single-point input (it's not a cell, so should be used as is instead of calling cell2mat)
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/MatlabBridge/src@970 25a7314f-c4c8-4314-9936-a25f8480a772

--- MatlabCommander/commandserver/cli_pointvectordecode.m
+++ MatlabCommander/commandserver/cli_pointvectordecode.m
@@ -1,7 +1,12 @@
 function points=cli_pointvectordecode(paramValue)
 % Creates a [3xN] matrix with the point positions in LPS coordinate system
 
-points=cell2mat(paramValue);
+if (iscell(paramValue))
+  points=cell2mat(paramValue);
+else
+  % Single fiducial point, it's not stored in a cell
+  points=paramValue;
+end
 
 % Point coordinates are provided by Slicer in RAS coordinate system
 % Multiply the first two components by -1 to obtain points in LPS coordinate system
