301c0cf817aa350bae4ae31e3026d81f299ee23a
lassoan <lassoan@25a7314f-c4c8-4314-9936-a25f8480a772>
2013-07-10 22:23:59 +0000

    test #360: Added missin quotation mark to proxy bat file template
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/MatlabBridge/src@999 25a7314f-c4c8-4314-9936-a25f8480a772

--- MatlabModuleGenerator/Resources/Templates/MatlabModuleTemplate.bat
+++ MatlabModuleGenerator/Resources/Templates/MatlabModuleTemplate.bat
@@ -18,7 +18,7 @@ if not defined SLICER_MATLAB_EXECUTABLE_PATH echo ERROR: SLICER_MATLAB_EXECUTABL
 rem Make the .bat file location to be the working directory (that's where the MatlabCommander.exe is)
 pushd "%~dp0"
 rem Forward parameters to the Matlab CLI
-"%SLICER_HOME%/Slicer.exe" --launcher-no-splash --launch %SLICER_MATLAB_COMMANDER_PATH% --call-matlab-function %MODULE_NAME% %*
+"%SLICER_HOME%/Slicer.exe" --launcher-no-splash --launch "%SLICER_MATLAB_COMMANDER_PATH%" --call-matlab-function %MODULE_NAME% %*
 if errorlevel 1 exit /b 1
 rem Return with success
 exit /b 0
