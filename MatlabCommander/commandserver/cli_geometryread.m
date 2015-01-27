f4fd0934a04fd8759decbff5bbc086fbe07efe6a
lassoan <lassoan@25a7314f-c4c8-4314-9936-a25f8480a772>
2015-01-27 05:27:45 +0000

    fixed #701: cli_geometryread.m and cli_geometrywrite.m use LPS coordinate system. This change is not backward compatible, code that used these .m files have to be updated.
    Also added a new function, cli_version.m for getting MatlabBridge version.
    
    git-svn-id: https://subversion.assembla.com/svn/slicerrt/trunk/MatlabBridge/src@2110 25a7314f-c4c8-4314-9936-a25f8480a772

--- MatlabCommander/commandserver/cli_geometryread.m
+++ MatlabCommander/commandserver/cli_geometryread.m
@@ -1,9 +1,9 @@
 function geometry = cli_geometryread(filename)
-%cli_geometryread  Read geometry for the command-line interface module from file (in STL format)
+%cli_geometryread  Read geometry for the command-line interface module from file (in STL format) into LPS coordinate system
 %
 %   geometry = cli_geometryread(filename) reads the mesh
 %
-%   geometry.vertices contains the vertices for all triangles [3*n x 3].
+%   geometry.vertices contains the vertices for all triangles in LPS coordinate system [3*n x 3].
 %   geometry.faces contains the vertex lists defining each triangle face [n x 3].
 % 
 %   Current limitations/caveats:
@@ -18,3 +18,6 @@ function geometry = cli_geometryread(filename)
 
 geometry.vertices = v;
 geometry.faces = f;
+
+% Slicer writes mesh points in RAS coordinate system by default, convert to LPS now
+geometry.vertices(:,1:2) = -1*geometry.vertices(:,1:2)
--- MatlabCommander/commandserver/cli_geometrywrite.m
+++ MatlabCommander/commandserver/cli_geometrywrite.m
@@ -1,10 +1,13 @@
 function cli_geometrywrite(outputFilename, geometry)
 % Function for writing mesh data to file
 %
-%   geometry.vertices contains the vertices for all triangles [3*n x 3].
+%   geometry.vertices contains the vertices for all triangles in LPS coordinate system [3*n x 3].
 %   geometry.faces contains the vertex lists defining each triangle face [n x 3].
 %
 %   Current limitations/caveats:
 %   * Only triangle mesh geometry is supported.
+
+% Slicer expects mesh points in RAS coordinate system by default, convert from LPS now
+geometry.vertices(:,1:2) = -1*geometry.vertices(:,1:2)
 
 stlwrite(outputFilename, geometry);
